328331d861d0c257f4ef7eb71ac0ab3d688cfaff
Frank Cuthbertson <fccuthbertson@gmail.com>
2020-04-25 16:02:52 -0500

    fixing file refs. Looks niiiice.

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,22 @@ const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname+'/../views/index.html'), { title: 'Express' });
+  res.sendFile(path.join(__dirname+'/../public/index.html'), { title: 'Express' });
 });
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.sendFile(path.join(__dirname+'/../views/about.html'), { title: 'Express' });
+  res.sendFile(path.join(__dirname+'/../public/about.html'), { title: 'Express' });
+});
+
+/* GET products page. */
+router.get('/products', function(req, res, next) {
+  res.sendFile(path.join(__dirname+'/../public/products.html'), { title: 'Express' });
+});
+
+/* GET products page. */
+router.get('/contact', function(req, res, next) {
+  res.sendFile(path.join(__dirname+'/../public/contact.html'), { title: 'Express' });
 });
 
 module.exports = router;
